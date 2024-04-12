ba496e9447315387f9bd722f1117cb4bd4bb67c4
ashvin-007 <parmarashvin9280@gmail.com>
2024-04-12 10:24:43 +0530

    add more logic

--- App.js
+++ App.js
@@ -1,46 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Appa.css';
 import Navbar from './componant/Navbar';
-import First from './componant/First';
 import Textform from './componant/Textform';
+import Alert from './componant/Alert';
+import About from './componant/About';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
+  const[mode,setMode]= useState('light');
+  const[alert,setAlert]=useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+       msg: message,
+       type: type
+    }
+   
+    )
+    setTimeout(() => {
+      setAlert(null);
+   }, 1500);
+  }
+
+
+    
+  const togglemode=()=>{
+        if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("This is dark mode enable","success");
+      document.title="Textutils-Dark Mode";
+      setInterval(() => {
+      document.title="Textutils is amazaing mode";
+        
+      }, 2000);
+      setInterval(() => {
+        document.title=" Install textutils";
+          
+        }, 1500);
+      
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor='white';
+      showAlert("This is light mode enable","success");
+      document.title="Textutils-Light Mode";
+
+      
+      
+
+
+    }
+  }
   return (
     <>
-  {/* <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">TextUtilies</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-       
-      </ul>
-      <form className="d-flex" role="search" >
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-    </div>
-</nav> */}
-{/* <Navbar title="TextUtils" about="TextAbout"/> */}
-{/* <Navbar title={3} about="TextAbout"/> */}
-
-<Navbar title="TextUtils" about="TextAbout"/>
+  <BrowserRouter>
+<Navbar title="TextUtils" about="TextAbout" mode={mode} togglemode={togglemode}/>
+<Alert alert={alert} />
 
 
-{/* <Navbar /> */}
-{/* <First name="Ashvin" /> */}
-<Textform heading="Enter text to analyze below"/>
-
-
-</>
+<Routes>
+  <Route exact path="/" element={<Textform heading="Enter text to analyze below" mode={mode} showAlert={showAlert}/>}></Route>
+</Routes>
+<Routes>
+  <Route exact path="/About" element={<About mode={mode}/>}></Route>
+</Routes>
+</BrowserRouter>
+</> 
   );
   
 }
--- Textform.js
+++ Textform.js
@@ -1,50 +1,60 @@
 import React,{useState} from 'react'
 export default function Textform(props) {
-    const [text,setText]=useState("Enter text here");
+    const [text,setText]=useState("");
        const handleUpClick=()=>{
             console.log("Handle Click" + text);
             const newText=text.toUpperCase();
             setText(newText);
+           props.showAlert("Convert to Uppercase!","success");
+
+            
      }
      const handleLoClick=()=>{
  
            const newText=text.toLowerCase();
            setText(newText);
+           props.showAlert("Convert to Lowercase!","success");
+
      }
      const handleClearClick=()=>
      {
 
          const newText='';
          setText(newText);
+         props.showAlert("Clear text!","success");
+
         }
+
      const handleOnChange=(event)=>{
         console.log("Handle Change");
         setText(event.target.value);
 
      }
      const handleCopy=()=>{
-           var text=document.getElementById('mybox');
-          // let newText
-          text.select();
-           navigator.clipboard.writeText(text.value);
-          // setText(navigator.clipboard.writeText(newText));
+        
+           navigator.clipboard.writeText(text);
+         
+          props.showAlert("Copy text to clipboard!","success");
+
      }
      const removeExtraSpaces=()=>{
         const  newText =text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Remove extra spaces!","success");
+
 
      }
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color:props.mode==='light'?'black':'white'}}>
       <h1>{props.heading} </h1>
       <div className="mb-3 ">
-  <textarea className="form-control mb-3" onChange={handleOnChange} id="mybox" value={text} rows="8"></textarea>
-  <button className="btn btn-primary mx=1" onClick={handleUpClick}>Convert To Upper Case</button>
-  <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To Lower Case</button>
-  <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear text</button>
-  <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy text</button>
-  <button className="btn btn-primary mx-1" onClick={removeExtraSpaces}>Remove Extra Spaces</button>
+  <textarea className="form-control mb-3" onChange={handleOnChange} id="mybox" value={text} rows="8" style={{backgroundColor:props.mode==='dark'?'grey':'white' ,color:props.mode==='light'?'black':'white'}} ></textarea>
+  <button disabled={text.length===0} className="btn btn-primary mx=1 my-1" onClick={handleUpClick}>Convert To Upper Case</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert To Lower Case</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy text</button>
+  <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={removeExtraSpaces}>Remove Extra Spaces</button>
 
 
 
@@ -52,12 +62,12 @@ export default function Textform(props) {
 
 </div>
     </div>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='light'?'black':'white'}}>
       <h1>Your text summary</h1>
-      <p>{text.split(" ").length} word and {text.length} character</p>
-      <p> {0.008*text.split(" ").length} Minutes read</p>
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} word and {text.length} character</p>
+      <p> {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
     </div>
     </>
   )
